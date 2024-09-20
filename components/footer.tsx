83e5c878853b4e9c4b0a05fa128344fcc9705c65
whatsup@lemonbase.com <whatsup@lemonbase.com>
2024-09-20 12:59:34 +0900

    feat: footer에도 예시로 적용

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,20 +1,24 @@
 import Link from "next/link";
+import { HStack, VStack } from "@/components/stactk";
+
+function StyledText({ children }: { children: React.ReactNode }) {
+  return <span className="text-headline-large">{children}</span>;
+}
 
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-between self-stretch bg-light-surfaceDim px-[10%] py-12 dark:bg-dark-surfaceDim">
-      <div className="flex flex-col gap-2">
-        <span className="text-headline-large">Team Thlee</span>
-        <span>Software Maestro 15th - 2024</span>
-      </div>
-      <div className="flex flex-col items-end gap-2">
-        <Link className="" href="https://github.com/SWM-Thlee">
-          Github
-        </Link>
-        <Link className="" href="mailto:cutehammond772@gmail.com">
-          Contact Us
-        </Link>
-      </div>
+    <footer className="self-stretch bg-light-surfaceDim px-[10%] py-12 dark:bg-dark-surfaceDim">
+      <HStack className="items-center justify-between self-stretch">
+        <VStack className="gap-2">
+          {/* StyledText를 적절한 네이밍의 Component로 규격화해서 관리할 수 있겠죠? 예를 들면, Body2 같은 네이밍으로요 */}
+          <StyledText>Team Thlee</StyledText>
+          <span>Software Maestro 15th - 2024</span>
+        </VStack>
+        <VStack className="items-end gap-2">
+          <Link href="https://github.com/SWM-Thlee">Github</Link>
+          <Link href="mailto:cutehammond772@gmail.com">Contact Us</Link>
+        </VStack>
+      </HStack>
     </footer>
   );
 }
