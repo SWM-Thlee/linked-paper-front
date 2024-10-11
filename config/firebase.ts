af0b0fc02fd9c87d1912188a7471be1b6c6a23a2
Jungheon Lee <cutehammond772@gmail.com>
2024-10-11 18:02:38 +0900

    feat: Firebase 적용

---
+++ config/firebase.ts
@@ -0,0 +1,22 @@
+"use client";
+
+import { initializeApp, FirebaseApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Firebase 초기화는 브라우저에서만 이루어짐
+const app: FirebaseApp = initializeApp(firebaseConfig);
+
+// Analytics는 브라우저 환경에서만 초기화
+const analytics = getAnalytics(app);
+
+export { analytics };
