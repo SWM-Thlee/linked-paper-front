cbb85e7f66038b372cc7c4052c47c75daba36a4c
Jungheon Lee <cutehammond772@gmail.com>
2024-10-25 12:03:30 +0900

    feat: TextArea Component 추가, Dialog Component 개선

--- ui/dialog/component.tsx
+++ ui/dialog/component.tsx
@@ -3,48 +3,28 @@
 import * as Primitive from "@radix-ui/react-dialog";
 
 import { tv, VariantProps } from "@/utils/style/tailwind-variants";
-import { sem } from "@/utils/style/semantic-styles";
 import CloseIcon from "../icons/close";
 
 export const dialogVariant = tv({
   slots: {
-    overlay: sem()
-      .layout(["fixed", "inset-0", "z-overlay"])
-      .color(["bg-light-shadow/15", "dark:bg-dark-shadow/50"])
-      .transition(["data-[state=open]:animate-overlayShow"])
-      .build(),
-    content: sem()
-      .layout([
-        "fixed",
-        "top-[50%] left-[50%]",
-        "min-w-[50vw] max-w-[90vw]",
-        "min-h-[50vh] max-h-[75vh]",
-        "translate-x-[-50%] translate-y-[-50%]",
-        "focus:outline-none",
-        "z-dialog",
-      ])
-      .color([
-        "bg-light-surfaceContainer",
-        "dark:bg-dark-surfaceContainer",
-        "text-light-onSurface",
-        "dark:text-dark-onSurface",
-        "shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]",
-      ])
-      .transition([
-        "data-[state=open]:animate-contentShow",
-        "data-[state=closed]:animate-contentHide",
-      ])
-      .build(),
-    close: sem()
-      .layout([
-        "absolute",
-        "inline-flex",
-        "appearance-none",
-        "justify-center",
-        "items-center",
-      ])
-      .color(["text-light-onSurface", "dark:text-dark-onSurface"])
-      .build(),
+    overlay: [
+      "fixed inset-0 z-overlay",
+      "bg-light-shadow/15 dark:bg-dark-shadow/50",
+      "transition data-[state=open]:animate-overlayShow",
+    ],
+    content: [
+      "fixed top-[50%] left-[50%]",
+      "translate-x-[-50%] translate-y-[-50%]",
+      "focus:outline-none z-dialog",
+      "bg-light-surfaceContainer dark:bg-dark-surfaceContainer",
+      "text-light-onSurface dark:text-dark-onSurface",
+      "shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]",
+      "transition data-[state=open]:animate-contentShow data-[state=closed]:animate-contentHide",
+    ],
+    close: [
+      "absolute inline-flex appearance-none justify-center items-center",
+      "text-light-onSurface dark:text-dark-onSurface",
+    ],
   },
   variants: {
     ui_size: {
@@ -69,10 +49,12 @@ export const dialogVariant = tv({
 
 export interface DialogRootProps extends Primitive.DialogProps {}
 
-export default function Root({ children, ...props }: DialogRootProps) {
+export function Root({ children, ...props }: DialogRootProps) {
   return <Primitive.Root {...props}>{children}</Primitive.Root>;
 }
 
+export const Title = Primitive.DialogTitle;
+export const Description = Primitive.DialogDescription;
 export const Trigger = Primitive.DialogTrigger;
 
 export interface DialogContentProps
